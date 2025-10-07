54162d067e57168514ab2dc255da7f65384aebc7
aazhnaspam <aashnaspam12@gmail.com>
2025-10-07 21:00:12 +0530

    backend changes - added frontend deployed url

--- backend/index.js
+++ backend/index.js
@@ -65,8 +65,7 @@ const PORT = process.env.PORT || 8000;
 // --- Apply ALL Middleware Here ---
 
 app.use(cors({
-    //origin: process.env.FRONTEND_URL,
-    origin : 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
 }));
 app.use(express.json());
